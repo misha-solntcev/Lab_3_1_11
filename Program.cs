9ea7a726c05bb2f27fb315e49de63ef476b07128
misha-solntcev <misha.solntcev@gmail.com>
2022-11-28 17:47:23 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 11. Дано целое число N (> 1). Вывести наименьшее из целых чисел K, 
+    для которых сумма 1 + 2 + … + K будет больше или равна N, 
+    и саму эту сумму. */
+
+namespace Lab_3_1_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 105;
+            int sum = 0;
+            int K = 0;
+            while(sum < N)
+            {
+                K++;
+                sum += K;                
+                Console.WriteLine($"K = {K}, sum = {sum} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
